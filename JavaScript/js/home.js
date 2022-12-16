07af60ad03ea57c82642d99d01a213bac21a6dd8
Sripathi Sandilya <sandycodesearch@gmail.com>
2022-12-16 17:06:16 +0530

    initial commit

---
+++ JavaScript/js/home.js
@@ -0,0 +1,10 @@
+let name = "Hiking Boots";
+let price = 49.99, discounted = false;
+showMessage(price);
+
+let costPrice = 99.99;
+//lot of code
+costPrice = 59.99;
+//more code
+costPrice = 29.99;
+showMessage(costPrice);
---
+++ JavaScript/js/home2.js
@@ -0,0 +1,2 @@
+var price = 25;
+showMessage(price);
